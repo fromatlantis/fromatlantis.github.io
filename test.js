bb0dbe56943c6d3fe1b6f1bfe51abffbe34568ed
牛志伟 <niuzhiwei@mininglamp.com>
2017-10-19 21:38:18 +0800

    test for XiaoMing

--- test.js
+++ test.js
@@ -2,3 +2,7 @@ document.addEventListener("click", function(e){
 	var target = e.target || e.srcElement;
 	alert(target.innerText);
 });
+var div = document.createElement("div");
+div.className = "loading";
+div.innerHTML = "<p>我是嵌入的内容</p>"
+window.document.body.appendChild(div);
