7d35226cc61ad3f7bf4b81d152a28b5e891bfa03
Vincent Neo <76011299@qq.com>
2017-12-31 14:48:39 +0800

    Update hui.js

--- hui.js
+++ hui.js
@@ -3,4 +3,5 @@ app.$notify({
   message: '努力过后，所有的一切交给时间和宿命。',
   position: 'bottom-right'
 });
-app.title = '嘿嘿，我存在于网络到达的每一个时间和空间';
+//app.title = '嘿嘿，我存在于网络到达的每一个时间和空间';
+app.title = '嘿嘿，宝宝明天我们去滑雪吧';
