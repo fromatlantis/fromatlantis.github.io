77e31ffdabf8d56feb7e44e21cce2b3a306a8972
Vincent Neo <76011299@qq.com>
2018-01-09 10:26:52 +0800

    Update hui.js

--- hui.js
+++ hui.js
@@ -3,5 +3,5 @@ app.$notify({
   message: '努力过后，所有的一切交给时间和宿命。',
   position: 'bottom-right'
 });
-//app.title = '嘿嘿，我存在于网络到达的每一个时间和空间';
-app.title = '一心一意，是世界上最温柔的力量';
+app.title = '嘿嘿，我存在于网络到达的每一个时间和空间，以及你的身边';
+//app.title = '一心一意，是世界上最温柔的力量';
